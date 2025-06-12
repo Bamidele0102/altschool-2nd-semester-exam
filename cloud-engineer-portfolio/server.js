99c15c0ee8e058ac92dd4180455b7dbc5777cb4a
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2025-06-12 03:09:36 +0100

    Install helmet for basic security headers

--- cloud-engineer-portfolio/server.js
+++ cloud-engineer-portfolio/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import helmet from 'helmet';
 
 const app = express();
 const port = 3000;
@@ -8,6 +9,9 @@ const port = 3000;
 // Needed for __dirname in ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+// Use helmet for security headers
+app.use(helmet());
 
 // Serve static files from /public
 app.use(express.static(path.join(__dirname, 'public')));
